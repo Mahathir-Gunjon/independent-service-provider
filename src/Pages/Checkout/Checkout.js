112cb3411257ab750a1a937c92fae5925330d0d5
Mahathir-Gunjon <mahathirgunjon@gmail.com>
2022-04-18 18:25:32 +0600

    about route create and developed

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -6,104 +6,105 @@ const Checkout = () => {
     return (
         <div className="bg-black text-white check-out-section d-flex align-items-center">
             <Container>
+                <h1 className='text-danger text-center display-3 py-5'>Check out</h1>
                 <div className="row">
                     <div className="col-md-8">
-                        <h4 class="mb-3">Payment</h4>
+                        <h4 className="mb-3">Payment</h4>
 
-                        <div class="my-3">
-                            <div class="form-check">
-                                <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked required />
-                                <label class="form-check-label" for="credit">Credit card</label>
+                        <div className="my-3">
+                            <div className="form-check">
+                                <input id="credit" name="paymentMethod" type="radio" className="form-check-input" required />
+                                <label className="form-check-label">Credit card</label>
                             </div>
-                            <div class="form-check">
-                                <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required />
-                                <label class="form-check-label" for="debit">Debit card</label>
+                            <div className="form-check">
+                                <input id="debit" name="paymentMethod" type="radio" className="form-check-input" required />
+                                <label className="form-check-label" >Debit card</label>
                             </div>
-                            <div class="form-check">
-                                <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required />
-                                <label class="form-check-label" for="paypal">PayPal</label>
+                            <div className="form-check">
+                                <input id="paypal" name="paymentMethod" type="radio" className="form-check-input" required />
+                                <label className="form-check-label">PayPal</label>
                             </div>
                         </div>
 
-                        <div class="row gy-3">
-                            <div class="col-md-6">
-                                <label for="cc-name" class="form-label">Name on card</label>
-                                <input type="text" class="form-control" id="cc-name" placeholder="" required />
-                                <small class="text-muted">Full name as displayed on card</small>
-                                <div class="invalid-feedback">
+                        <div className="row gy-3">
+                            <div className="col-md-6">
+                                <label className="form-label">Name on card</label>
+                                <input type="text" className="form-control" id="cc-name" placeholder="" required />
+                                <small className="text-muted">Full name as displayed on card</small>
+                                <div className="invalid-feedback">
                                     Name on card is required
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
-                                <label for="cc-number" class="form-label">Credit card number</label>
-                                <input type="text" class="form-control" id="cc-number" placeholder="" required />
-                                <div class="invalid-feedback">
+                            <div className="col-md-6">
+                                <label className="form-label">Credit card number</label>
+                                <input type="text" className="form-control" id="cc-number" placeholder="" required />
+                                <div className="invalid-feedback">
                                     Credit card number is required
                                 </div>
                             </div>
 
-                            <div class="col-md-3">
-                                <label for="cc-expiration" class="form-label">Expiration</label>
-                                <input type="text" class="form-control" id="cc-expiration" placeholder="" required />
-                                <div class="invalid-feedback">
+                            <div className="col-md-3">
+                                <label className="form-label">Expiration</label>
+                                <input type="text" className="form-control" id="cc-expiration" placeholder="" required />
+                                <div className="invalid-feedback">
                                     Expiration date required
                                 </div>
                             </div>
 
-                            <div class="col-md-3">
-                                <label for="cc-cvv" class="form-label">CVV</label>
-                                <input type="text" class="form-control" id="cc-cvv" placeholder="" required />
-                                <div class="invalid-feedback">
+                            <div className="col-md-3">
+                                <label className="form-label">CVV</label>
+                                <input type="text" className="form-control" id="cc-cvv" placeholder="" required />
+                                <div className="invalid-feedback">
                                     Security code required
                                 </div>
                             </div>
                         </div>
 
-                        <button class="w-100 btn btn-danger btn-lg my-5" type="submit">Continue to checkout</button>
+                        <button className="w-100 btn btn-danger btn-lg my-5" type="submit">Continue to checkout</button>
                     </div>
                     <div className="col-md-4">
-                        <h4 class="d-flex justify-content-between align-items-center mb-3">Your cart
+                        <h4 className="d-flex justify-content-between align-items-center mb-3">Your cart
                         </h4>
-                        <ul class="list-group mb-3">
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
+                        <ul className="list-group mb-3">
+                            <li className="list-group-item d-flex justify-content-between lh-sm">
                                 <div>
-                                    <h6 class="my-0">Product name</h6>
-                                    <small class="text-muted">Brief description</small>
+                                    <h6 className="my-0">Product name</h6>
+                                    <small className="text-muted">Brief description</small>
                                 </div>
-                                <span class="text-muted">$12</span>
+                                <span className="text-muted">$12</span>
                             </li>
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
+                            <li className="list-group-item d-flex justify-content-between lh-sm">
                                 <div>
-                                    <h6 class="my-0">Second product</h6>
-                                    <small class="text-muted">Brief description</small>
+                                    <h6 className="my-0">Second product</h6>
+                                    <small className="text-muted">Brief description</small>
                                 </div>
-                                <span class="text-muted">$8</span>
+                                <span className="text-muted">$8</span>
                             </li>
-                            <li class="list-group-item d-flex justify-content-between lh-sm">
+                            <li className="list-group-item d-flex justify-content-between lh-sm">
                                 <div>
-                                    <h6 class="my-0">Third item</h6>
-                                    <small class="text-muted">Brief description</small>
+                                    <h6 className="my-0">Third item</h6>
+                                    <small className="text-muted">Brief description</small>
                                 </div>
-                                <span class="text-muted">$5</span>
+                                <span className="text-muted">$5</span>
                             </li>
-                            <li class="list-group-item d-flex justify-content-between bg-light">
-                                <div class="text-success">
-                                    <h6 class="my-0">Promo code</h6>
+                            <li className="list-group-item d-flex justify-content-between bg-light">
+                                <div className="text-success">
+                                    <h6 className="my-0">Promo code</h6>
                                     <small>EXAMPLECODE</small>
                                 </div>
-                                <span class="text-success">$5</span>
+                                <span className="text-success">$5</span>
                             </li>
-                            <li class="list-group-item d-flex justify-content-between">
+                            <li className="list-group-item d-flex justify-content-between">
                                 <span>Total (USD)</span>
                                 <strong>$20</strong>
                             </li>
                         </ul>
 
-                        <form class="card p-2 mt-5">
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Promo code" />
-                                <button type="submit" class="btn btn-secondary">Redeem</button>
+                        <form className="card p-2 mt-5">
+                            <div className="input-group">
+                                <input type="text" className="form-control" placeholder="Promo code" />
+                                <button type="submit" className="btn btn-secondary">Redeem</button>
                             </div>
                         </form>
                     </div>
