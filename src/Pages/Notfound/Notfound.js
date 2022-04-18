6debb2a7b9a752a1a695ee9529fcfcded61667e3
Mahathir-Gunjon <mahathirgunjon@gmail.com>
2022-04-18 20:12:56 +0600

    404 page setup done

--- src/Pages/Notfound/Notfound.js
+++ src/Pages/Notfound/Notfound.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Notfound = () => {
     return (
-        <div>
-            khujio na ulta palta kisu
+        <div className='w-100' style={{height: 'auto'}}>
+            <img className='w-100' src='https://kfg6bckb.media.zestyio.com/yalantis-interactive-404.gif' alt="" />
         </div>
     );
 };
